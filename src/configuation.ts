770107f7ff28f47b55e69382a9552bd4c693ac6c
poohbear <452363993@qq.com>
2023-11-25 18:24:10 +0800

    feat: 使用joi对环境配置参数进行校验

--- src/configuation.ts
+++ src/configuation.ts
@@ -2,9 +2,21 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 import * as yaml from 'js-yaml';
 import * as _ from 'lodash';
+import * as Joi from 'joi';
 
-const YAML_CONFIG_FILENAME = 'config.yml';
-const commonPath = join(__dirname, '../', 'config', YAML_CONFIG_FILENAME);
+// 1. 定义环境参数类型
+const schema = Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production')
+    .default('development'),
+  db: Joi.object({
+    mysql1: Joi.object({
+      host: Joi.string().ip(),
+    }),
+  }),
+});
+
+const commonPath = join(__dirname, '../', 'config', 'config.yml');
 
 const envPath = join(
   __dirname,
@@ -18,5 +30,16 @@ const envConfig = yaml.load(readFileSync(envPath, 'utf8'));
 
 // 因为ConfigModule有load方法，因此这里导出一个函数
 export default () => {
-  return _.merge(commonConfig, envConfig);
+  const values = _.merge(commonConfig, envConfig);
+
+  // 2.对环境参数进行校验
+  const { error } = schema.validate(values, {
+    allowUnknown: true,
+    abortEarly: false,
+  });
+  if (error) {
+    throw new Error(`环境参数校验错误-${error.message}`);
+  }
+
+  return values;
 };
