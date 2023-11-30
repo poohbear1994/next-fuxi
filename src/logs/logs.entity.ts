16aca00a32f1f40e9a454f9499cb2d4e768ffcf3
poohbear <452363993@qq.com>
2023-11-30 22:33:33 +0800

    feat: 创建1对多/多对1关系

--- src/logs/logs.entity.ts
+++ src/logs/logs.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import User from 'src/user/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 class Logs {
@@ -16,6 +17,9 @@ class Logs {
 
   @Column()
   result: number;
+
+  @ManyToOne(() => User, (user) => user.logs)
+  user: User;
 }
 
 export default Logs;
--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,13 +1,19 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import Logs from 'src/logs/logs.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 class User {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   username: string;
+
   @Column()
   password: string;
+
+  @OneToMany(() => Logs, (log) => log.user)
+  logs: Logs[];
 }
 
 export default User;
